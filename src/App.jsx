61186f13959c119e8c778ba7ea5522c10a053d73
MauroDS <mauro.diaz.santilli@gmail.com>
2023-07-13 14:43:41 -0300

    Creando rama para footer e instalando dependencia FontAwesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Menu></Menu>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
