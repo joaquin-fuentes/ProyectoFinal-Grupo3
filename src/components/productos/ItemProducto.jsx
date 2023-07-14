b224a211844bc7d299e61820ff3cb244790e1b70
Julian <julianser@hotmail.com.ar>
2023-07-14 17:15:55 -0300

    Se agregaron datos estaticos a la tabla

--- src/components/productos/ItemProducto.jsx
+++ src/components/productos/ItemProducto.jsx
@@ -5,7 +5,12 @@ import {FaSquareXmark, FaPenToSquare} from 'react-icons/fa6'
 const ItemProducto = () => {
     return (
         <tr>
-            
+            <td>Milanesa con Papas Fritas</td>
+            <td>$2000</td>
+            <td>10</td>
+            <td>Al Plato</td>
+            <td><Link className='btn btn-warning mb-2 mb-md-0' to={`/administradorProductos/editar`}><FaPenToSquare className='fs-4'></FaPenToSquare></Link>
+            <Button className='ms-md-2' variant='danger'><FaSquareXmark className='fs-4'></FaSquareXmark></Button></td>
         </tr>
     );
 };
