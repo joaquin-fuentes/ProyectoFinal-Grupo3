17f887874d19c8ed7e6454cba0cfef72decb3d1b
Julian <julianser@hotmail.com.ar>
2023-07-14 18:19:40 -0300

    Se agrego un boton para agregar producto en administrador de productos

--- src/components/productos/AdminProductos.jsx
+++ src/components/productos/AdminProductos.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Container, Table } from "react-bootstrap";
+import { Container, Table, Button } from "react-bootstrap";
 import ItemProducto from "./ItemProducto";
+import { Link } from "react-router-dom";
 
 const AdminProductos = () => {
   return (
     <Container className="mainSection my-4">
       <h1 className="display-4">Administrador de Productos</h1>
       <hr></hr>
+      <Link to={'/administradorProductos/crear'}><Button className="btn btn-primary mb-3 rounded-pill">Agregar Producto</Button></Link>
       <section className="table-responsive">
         <Table bordered hover className="table-dark">
           <thead>
