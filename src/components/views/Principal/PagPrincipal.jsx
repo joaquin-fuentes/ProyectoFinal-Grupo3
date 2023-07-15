559c097ec561731ae69a8eab4518344ad6091d3e
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-15 19:15:15 -0300

    Se agregan los componentes PagPrincipal.jsx y LandingPage.jsx, se desarrolla la landing

---
+++ src/components/views/Principal/PagPrincipal.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import LandingPage from "./LandingPage";
+
+const PagPrincipal = () => {
+    return (
+        <Container>
+            <LandingPage></LandingPage>
+        </Container>
+    );
+};
+
+export default PagPrincipal;
