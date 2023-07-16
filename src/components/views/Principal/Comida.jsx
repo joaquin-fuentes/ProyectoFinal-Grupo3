b8277c7d12ad1bf1efcb3857137f695ab8d8d88e
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-15 21:52:23 -0300

    Se agrega el maquetado de las cards de comidas

---
+++ src/components/views/Principal/Comida.jsx
@@ -0,0 +1,35 @@
+import { Card, Col, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Comida = () => {
+  return (
+    <Col className="mt-2">
+      <Card>
+        <div className="row g-0">
+          <div className="col-md-5">
+            <Image
+              src="https://clasicoygourmet.com/wp-content/uploads/2017/02/Pechugas-de-pollo-rellenas-FUENTE-ABCdesevilla.com_-678x470.jpg"
+              className="img-fluid rounded-start h-100 object-fit-cover"
+              alt="..."
+            ></Image>
+          </div>
+          <div className="col-md-7">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <Link id="btn-comida" className="btn">
+                Agregar al pedido
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </Col>
+  );
+};
+
+export default Comida;
--- src/components/views/Principal/PagPrincipal.jsx
+++ src/components/views/Principal/PagPrincipal.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import LandingPage from "./LandingPage";
 import ContenedorCategorias from "./ContenedorCategorias";
 import FormularioBuscador from "./FormularioBuscador";
+import ContenedorComidas from "./ContenedorComidas";
 
 const PagPrincipal = () => {
     return (
@@ -9,6 +10,7 @@ const PagPrincipal = () => {
             <LandingPage></LandingPage>
             <ContenedorCategorias></ContenedorCategorias>
             <FormularioBuscador></FormularioBuscador>
+            <ContenedorComidas></ContenedorComidas>
         </Container>
     );
 };
