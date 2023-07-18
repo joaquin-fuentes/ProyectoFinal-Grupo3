b97d84c8e414a1549d9c92547b916003fd2d74bc
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-07-18 18:31:54 -0300

    maquetado de  el archivoLogin.jsx

---
+++ src/components/views/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+    <Container className="mainSection">
+        <Card className="my-5 cardPrincipal">
+            <Card.Header as="h5" className='headLine'>Login</Card.Header>
+            <Card.Body>
+                <Form>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Ingrese el email" />
+                    <Form.Text className="text-danger">
+                    </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Mayor a 8 caracteres, debe tener una mayuscula y numeros" />
+                    <Form.Text className="text-danger">
+                    </Form.Text>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className='botonRegistrar'
+                    onMouseOver={(e) => (e.target.style.backgroundColor = "#654321")}
+                    onMouseOut={(e) => (e.target.style.backgroundColor = "#8c7851")}>
+                    Ingresar
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    </Container>
+    );
+};
+
+export default Login;
