18691688aa9215873ede299f17931f2df8b0f6bf
Julian <julianser@hotmail.com.ar>
2023-07-20 13:40:01 -0300

    Se creo la funcion que crea un producto

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,1 +1,17 @@
-console.log('xd')
+const URL_PRODUCTO = import.meta.env.VITE_API_PRODUCTO
+
+
+const crearProducto = async (producto) => {
+    try {
+        const respuesta = await fetch(URL_PRODUCTO,{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return respuesta
+    } catch (error) {
+        console.log(error)
+    }
+}
