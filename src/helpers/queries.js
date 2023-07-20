6554632d5cf5d6c5cd5238035042c6af49518b62
MauroDS <mauro.diaz.santilli@gmail.com>
2023-07-20 18:13:58 -0300

    Creando nueva rama para login

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,6 +1,31 @@
 const URL_PRODUCTO = import.meta.env.VITE_API_PRODUCTO
 const URL_USUARIO = import.meta.env.VITE_API_USUARIO
 
+export const login = async (usuario) =>{
+    try {
+      console.log(usuario);
+      const respuesta = await fetch(URL_USUARIO, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(usuario),
+      });
+      const datos = await respuesta.json();
+      return {
+        status: respuesta.status,
+        mensaje: datos.mensaje,
+        usuario: datos.nombre,
+        uid: datos.uid
+      };
+
+    
+    
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
 
 export const crearProducto = async (producto) => {
     try {
