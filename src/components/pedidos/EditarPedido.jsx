f4b43c27b79a6ed2103f6bc00fc5bf45a8ee8569
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-07-26 19:03:01 -0300

    logica para administrador de pedidos, para dividirlos segun estado, ya sean pendientes o realizados

--- src/components/pedidos/EditarPedido.jsx
+++ src/components/pedidos/EditarPedido.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 import { consultaEditarPedido, obtenerPedido } from "../../helpers/queries";
 
 const EditarPedido = () => {
+  const [productosDelMenu , setProductosDelMenu] = useState([])
   const {id}= useParams();
   const navegacion = useNavigate();
   const {
@@ -23,11 +24,14 @@ const EditarPedido = () => {
       setValue('fecha', respuesta.fecha)
       setValue('estado', respuesta.estado)
       setValue('nota', respuesta.nota)
+      setProductosDelMenu(respuesta.productosDelMenu)
     })
   }, [])
 
   const onSubmit = (pedidoEditado) =>{
-    consultaEditarPedido(pedidoEditado, id).then((respuesta)=>{
+    console.log(productosDelMenu)
+    const pedidoCompleto = {...pedidoEditado, productosDelMenu}
+    consultaEditarPedido(pedidoCompleto, id).then((respuesta)=>{
       if (respuesta) {
         if (respuesta.status === 200) {
           Swal.fire('Pedido actualizado', `El pedido: ${pedidoEditado.fecha} fue editado correctamente`, 'success');
@@ -90,7 +94,7 @@ const EditarPedido = () => {
                 message: "La nota debe tener como máximo 30 carácteres",
               },
               pattern: {
-                value: /^([A-Z\u00D1])[A-Za-z0-9:,.\s\u00D1\u00F1]{1,29}$/,
+                value: /^[A-Za-z\u00D1\u00F1][A-Za-z0-9:,.\s\u00D1\u00F1]{1,29}$/,
                 message:
                   "La nota debe ser corta entre 2 y 30 carácteres (puede usar letras, numeros y signos de puntuacion",
               },
--- src/components/pedidos/ItemPedido.jsx
+++ src/components/pedidos/ItemPedido.jsx
@@ -3,37 +3,41 @@ import { FaPenToSquare } from "react-icons/fa6";
 import { HiX } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-import { borrarPedido, obtenerPedidos, obtenerProductos } from "../../helpers/queries";
+import { borrarPedido, obtenerPedidos, obtenerProductos, consultaEditarPedido } from "../../helpers/queries";
 import { useEffect, useState } from "react";
 
-const ItemPedido = ({ pedido, setPedidos }) => {
+const ItemPedido = ({ pedido, setPedidos, actualizarPedidos }) => {
 
   const [productos, setProductos] = useState([])
+  const [pedidoEstado, setPedidoEstado] = useState(pedido);
 
-  useEffect(()=>{
+  useEffect(() => {
     obtenerProductos().then((respuesta) => {
-      if(respuesta)
-      {
+      if (respuesta) {
         setProductos(respuesta);
       }
-      else{
+      else {
         Swal.fire(
           'Se produjo un error al intentar cargar los datos',
           `Intente realizar esta operacion mas tarde`,
           'error');
       }
     })
-  },[])
+    verificarEstado()
+  }, [pedidoEstado])
 
   const calcularTotal = () => {
     let total = 0;
-    pedido.productosDelMenu.forEach((idProducto) => {
-      productos.forEach((productoDB)=>{
-        if(productoDB.id === idProducto){
-          total = total + +productoDB.precio
-        }
-      })
-    });
+    // Verificar si los productos se han cargado
+    if (productos.length > 0) {
+      pedido.productosDelMenu.forEach((idProducto) => {
+        productos.forEach((productoDB) => {
+          if (productoDB.id === idProducto) {
+            total = total + +productoDB.precio;
+          }
+        });
+      });
+    }
     return total;
   };
 
@@ -58,7 +62,7 @@ const ItemPedido = ({ pedido, setPedidos }) => {
               "success"
             );
             obtenerPedidos().then((respuesta) => {
-            setPedidos(respuesta);
+              setPedidos(respuesta);
             })
           } else {
             Swal.fire("Se produjo un error", "Intentelo mas tarde", "error");
@@ -68,10 +72,60 @@ const ItemPedido = ({ pedido, setPedidos }) => {
     });
   };
 
+  const realizarPedido = () => {
+    Swal.fire({
+      title: "¿Estas seguro?",
+      text: "Seguro que el pedido ya fue realizado?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, estoy seguro",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const pedidoEditado = { ...pedidoEstado, estado: "realizado" };
+        consultaEditarPedido(pedidoEditado, pedido.id).then((respuesta) => {
+          if (respuesta && respuesta.status === 200) {
+            setPedidoEstado(pedidoEditado); // Actualiza el estado de pedidoEstado
+            Swal.fire(
+              "Accion realizada",
+              `El pedido fue marcado como realizado`,
+              "success"
+            );
+            actualizarPedidos(); // Llama a la función para actualizar los pedidos
+          } else {
+            Swal.fire(
+              "Se produjo un error",
+              `El pedido no fue realizado, intentelo mas tarde`,
+              "error"
+            );
+          }
+        })
+
+      }
+    });
+  }
+
+  const verificarEstado = () => {
+    if (pedidoEstado.estado === "pendiente") {
+      return <Button className="btn-primary" onClick={realizarPedido}>Pendiente...</Button>;
+    } else {
+      return <Button className="btn-success" disabled >Realizado</Button>;
+    }
+
+  };
+  const manejadorColorCard = () => {
+    if (pedido.estado === "pendiente") {
+      return "colorCard m-2"
+    } else {
+      return "bg-primary text-white m-2"
+    }
+  }
   return (
     <Col xs={12} md={6} lg={6}>
-      <Card className="mt-3">
-        <Card.Header className="colorCard">
+      <Card className={manejadorColorCard()}>
+        <Card.Header>
           <span className="d-flex justify-content-end">
             <Button variant="danger">
               <HiX className="fs-4" onClick={eliminarPedido}></HiX>
@@ -79,33 +133,35 @@ const ItemPedido = ({ pedido, setPedidos }) => {
           </span>
           <h3 className="text-start">Pedido</h3>
         </Card.Header>
-        <Card.Body className="colorCard">
+        <Card.Body>
           <h6>Nombre de Usuario: {pedido.usuario}</h6>
           <h6>Fecha: {pedido.fecha}</h6>
           <h6>Pedido:</h6>
           <ul className="ps-5">
             {pedido.productosDelMenu.map((idProducto, index) => (
-              <li key={index}>{productos.map((productoDB)=>{if(productoDB.id === idProducto){
-                return productoDB.nombreProducto
-              }})} - ${productos.map((productoDB)=>{if(productoDB.id === idProducto){
-                return productoDB.precio
-              }})} </li>
+              <li key={index}>{productos.map((productoDB) => {
+                if (productoDB.id === idProducto) {
+                  return productoDB.nombreProducto
+                }
+              })} - ${productos.map((productoDB) => {
+                if (productoDB.id === idProducto) {
+                  return productoDB.precio
+                }
+              })} </li>
             ))}
           </ul>
           <h6>Nota: {pedido.nota}</h6>
           <h6 className="text-end">Total: ${calcularTotal()}</h6>
         </Card.Body>
-        <Card.Footer className="justify-content-end d-flex colorCard">
-          {/* <Button className="disabled btn-secondary me-md-2">
-            Pendiente...
-          </Button> */}
-        <Link
-          className="btn btn-warning me-2"
-          to={`/administrador/pedidos/editar/${pedido.id}`}
-        >
-          <FaPenToSquare className="fs-4"></FaPenToSquare>
-        </Link>
-          <Button className="btn-success">Realizado</Button>
+        <Card.Footer className="justify-content-end d-flex">
+          <Link
+            className="btn btn-warning me-2"
+            to={`/administrador/pedidos/editar/${pedido.id}`}
+          >
+            <FaPenToSquare className="fs-4"></FaPenToSquare>
+          </Link>
+          {verificarEstado()}
+
         </Card.Footer>
       </Card>
     </Col>
