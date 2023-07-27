ee60dbf1bdd3fc80dda5e459fbc2c2699ffa1552
Julian <julianser@hotmail.com.ar>
2023-07-27 20:45:51 -0300

    Se realizo la union con usuarios, productos y filtro de categorias

--- src/components/views/Principal/Categoria.jsx
+++ src/components/views/Principal/Categoria.jsx
@@ -3,8 +3,10 @@ import { Button } from "react-bootstrap";
 const Categoria = ({categoria , setCategoriaBuscada}) => {
   
   const filtroCategoria = () =>{
-    setCategoriaBuscada(categoria)
+    const categoriaMinuscula = categoria.toLowerCase();
+    setCategoriaBuscada(categoriaMinuscula)
   }
+  
   return (
     <th>
       <Button className="categoria my-4 d-flex justify-content-center" onClick={filtroCategoria}>
