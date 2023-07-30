a571121255ed9eee9aa82ce8756c2a705e60c48e
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-07-30 20:05:06 -0300

    se arregleo estilos de las card de inicio

--- src/components/views/Principal/Comida.jsx
+++ src/components/views/Principal/Comida.jsx
@@ -62,22 +62,22 @@ const Comida = ({ producto, setHayProductosEnPedido }) => {
   return (
     <Col className="mt-2">
       <Card className="h-100">
-        <div className="row g-0">
-          <div className="col-md-5">
+        <div className="row g-0 h-100">
+          <div className="col-md-5 ">
             <Image
               src={producto.imagen}
-              className="imagenCard w-100 rounded-start object-fit-cover h-100"
+              className="imagenCard w-100 h-100 rounded-start object-fit-cover h-100"
               alt={producto.nom}
             ></Image>
           </div>
-          <div className="col-md-7">
-            <div className="card-body">
-              <h5 className="card-title">{producto.nombreProducto}</h5>
-              <p className="card-text">
+          <div className="col-md-7 cuerpoInfoCardProducto">
+            <div className="card-body cuerpoInfoCardProductoInterno">
+              <h5 className="card-title mb-auto">{producto.nombreProducto}</h5>
+              <p className="card-text detalleProductoInicio mb-auto">
                 {producto.detalle}
               </p>
-              <h5 className="card-title" id="precio-comida">$ {producto.precio}</h5>
-              <Link to={`/detalleProducto/${producto._id}`} id="btn-verdetalle" className="btn w-100 mb-2" >Ver detalle</Link>
+              <h5 className="card-title mb-auto" id="precio-comida">$ {producto.precio}</h5>
+              <Link to={`/detalleProducto/${producto._id}`} id="btn-verdetalle" className="btn w-100 mb-2 mt-auto" >Ver detalle</Link>
               {verificarDisponibilidad()}
             </div>
           </div>
