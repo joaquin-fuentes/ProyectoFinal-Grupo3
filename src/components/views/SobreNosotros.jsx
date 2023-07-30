9845d10d2882de8497dec3e363377b8f7468abb5
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-30 18:13:24 -0300

    Se cambia el logo y se corrige la pagina sobrenosotros

--- src/components/views/SobreNosotros.jsx
+++ src/components/views/SobreNosotros.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import brand from "../../assets/brand.png"
 import chef1 from "../../assets/chefs/MauroDiazSantilli.jpeg"
@@ -12,27 +11,21 @@ const AcercaDe = () => {
   return (
     <Container className="mainSection">
       <section className="my-5">
-        <h1 className="display-4 text-center pb-3">¿Que es JSONGourmet?</h1>
+        <h1 className="display-4 text-center pb-3">¿Que es Bon dia?</h1>
         <p className="text-center fw-bold fst-italic texto-acerca">
-          Uno de los restaurantes mas famosos de Argentina, claro está. Nuestro objetivo es ofrecer una experiencia gastronómica única e inigualable a nuestros clientes. Nos apasiona la comida de calidad y nos esforzamos por brindar platos exquisitos y creativos que satisfagan los paladares más exigentes.
+        Uno de los restaurantes más famosos de Argentina, conocido por ofrecer una experiencia gastronómica única e inigualable. Su pasión por la comida de calidad se refleja en platos exquisitos y creativos que satisfacen los paladares más exigentes.
         </p>
         <p className="text-center fw-semibold texto-acerca">
-          Nuestro restaurante ofrece una amplia variedad de opciones gastronómicas, desde platos tradicionales hasta creaciones innovadoras de nuestra cocina. Utilizamos ingredientes frescos y de temporada para garantizar el sabor y la calidad en cada plato que servimos. Por supuesto, no podemos olvidarnos de nuestros beberajes. Ofrecemos una amplia variedad de bebidas, tanto locales como internacionales.
+        Los usuarios pueden explorar el menú, conocer a los chefs y realizar pedidos en línea a través de su página web. Bon dia espera darles la bienvenida y brindarles una experiencia culinaria única e inigualable.
         </p>
-        <p className="text-center fw-semibold texto-acerca">
-          Además de nuestra comida y bebida, en JSONGourmet nos enorgullece ofrecer un ambiente acogedor y un servicio excepcional. Queremos que cada visita a nuestro restaurante sea una experiencia memorable para nuestros comensales, donde puedan disfrutar de una atención personalizada y un ambiente agradable.
-        </p>
-        <p className="text-center fw-semibold texto-acerca">
-          En nuestra página web, los usuarios podrán explorar nuestro menú, conocer más sobre nuestros chefs y reservar un pedido en línea.  ¡Esperamos darles la bienvenida y hacer que su experiencia culinaria sea unica e inigualable!
-        </p>
-        <img src={brand} alt="Logo JSON" className="brandJSON" />
+        <img src={brand} alt="Logo Bon dia" className="brandBondia" />
       </section>
       <hr />
-      <section className="bg-body-tertiary fondo-acerca">
-        <h1 className="display-6 text-center mb-4 pt-4 text-warning fw-bolder">Nuestros chefs</h1>
+      <section className="my-5 rounded">
+        <h1 className="display-6 text-center mb-4 pt-4 fw-bolder">Nuestros chefs</h1>
         <Row className="sobreNosotros-card">
           <Col md={5} className="mb-3 mx-1 p-1">
-            <Card>
+            <Card className='border-0'>
               <Row className="g-0">
                 <Col md={4}>
                   <Card.Img
@@ -53,7 +46,7 @@ const AcercaDe = () => {
             </Card>
           </Col>
           <Col md={5} className="mb-3 mx-1 p-1">
-            <Card>
+            <Card className='border-0' className='border-0'>
               <Row className="g-0">
                 <Col md={4}>
                   <Card.Img
@@ -74,7 +67,7 @@ const AcercaDe = () => {
             </Card>
           </Col>
           <Col md={5} className="mb-3 mx-1 p-1">
-            <Card>
+            <Card className='border-0' className='border-0'>
               <Row className="g-0">
                 <Col md={4}>
                   <Card.Img
@@ -95,7 +88,7 @@ const AcercaDe = () => {
             </Card>
           </Col>
           <Col md={5} className="mb-3 mx-1 p-1">
-            <Card>
+            <Card className='border-0' className='border-0'>
               <Row className="g-0">
                 <Col md={4}>
                   <Card.Img
@@ -116,7 +109,7 @@ const AcercaDe = () => {
             </Card>
           </Col>
           <Col md={5} className="mb-3 mx-1 p-1">
-            <Card>
+            <Card className='border-0'>
               <Row className="g-0">
                 <Col md={4}>
                   <Card.Img
