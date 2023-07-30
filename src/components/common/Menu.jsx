1385f3336549acc91bd72dc6ec3ce8f8a1e98923
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-30 01:44:02 -0300

    Se corrigen los estilos del navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Navbar, Container, Nav, Button, NavDropdown } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { FaArrowUp, FaUtensils } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 import "../../App.css";
-import Swal from 'sweetalert2';
-import 'sweetalert2/dist/sweetalert2.css';
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 
 const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
   const navegar = useNavigate();
 
   const logout = () => {
     Swal.fire({
-      title: '¿Está seguro?',
-      text: '¿Desea cerrar sesión?',
-      icon: 'warning',
+      title: "¿Está seguro?",
+      text: "¿Desea cerrar sesión?",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí, cerrar sesión',
-      cancelButtonText: 'Cancelar'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, cerrar sesión",
+      cancelButtonText: "Cancelar",
     }).then((result) => {
       if (result.isConfirmed) {
-        sessionStorage.removeItem('usuario');
-        sessionStorage.removeItem('productosEnPedido');
+        sessionStorage.removeItem("usuario");
+        sessionStorage.removeItem("productosEnPedido");
         setUsuarioLogueado({});
-        navegar('/');
+        navegar("/");
       }
     });
-  }
+  };
 
   const handleScroll = () => {
     const button = document.getElementById("boton-arriba");
@@ -45,7 +46,6 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
 
   return (
     <>
@@ -66,26 +66,41 @@ const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
               <NavLink className="nav-item nav-link" to={"/pedido"}>
                 <FaUtensils className="fs-4" />
               </NavLink>
-               
+
               {usuarioLogueado.uid ? (
                 <>
                   {usuarioLogueado.isAdmin === true ? (
                     <NavDropdown title="Administrador" id="admin-dropdown">
-                      <NavDropdown.Item as={NavLink} to={"/administrador/productos"}>
+                      <NavDropdown.Item
+                        as={NavLink}
+                        to={"/administrador/productos"}
+                      >
                         Productos
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={NavLink} to={"/administrador/usuarios"}>
+                      <NavDropdown.Item
+                        as={NavLink}
+                        to={"/administrador/usuarios"}
+                      >
                         Usuarios
                       </NavDropdown.Item>
-                      <NavDropdown.Item as={NavLink} to={"/administrador/pedidos"}>
+                      <NavDropdown.Item
+                        as={NavLink}
+                        to={"/administrador/pedidos"}
+                      >
                         Pedidos
                       </NavDropdown.Item>
                     </NavDropdown>
                   ) : null}
 
-                  <Button variant="dark" onClick={logout} className="btn-cerrar-sesion">
-                    Cerrar Sesion
+                  <div>
+                  <Button
+                    variant="dark"
+                    onClick={logout}
+                    id="btn-cerrar-sesion"
+                  >
+                    <FiLogOut></FiLogOut>
                   </Button>
+                  </div>
                 </>
               ) : (
                 <>
