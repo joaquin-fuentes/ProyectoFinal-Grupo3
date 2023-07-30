81db2d71dd9c00d620dead1a77d8a25104e8b7d3
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-30 20:10:38 -0300

    Se corrige el error404 y un padding en el navbar

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap';
-import error from '../../assets/Error404.gif';
+import error from '../../assets/Error404.png';
 import '../../App.css';
 
 const Error404 = () => {
   return (
     <section className="mainSection text-center">
       <img src={error} alt="error 404" className="error404-img" />
-      <p className="error404-texto">Ups, nos atrapó. La página a la que intenta acceder no se encuentra disponible. 
-        Para volver a la página principal, haga clic en el botón de abajo.</p>
+      <p className="error404-texto">Ups, error al cargar la pagina.</p>
         <div className="error404-contenido">
         <Link to="/">
-          <Button variant="danger">Volver al inicio</Button>
+          <Button id='btn-error404'>Volver al inicio</Button>
         </Link>
       </div>
     </section>
